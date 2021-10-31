fe2cb0e7b482e8dccab25009bb82933f7c207337
Gokhan <gokhanozgur93@gmail.com>
2021-11-01 00:00:50 +0300

    Which data in token and tokens lifetime declaration development.

--- IdentityServer/XProject.IdentityServer/Config.cs
+++ IdentityServer/XProject.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace XProject.IdentityServer
@@ -20,10 +21,19 @@ namespace XProject.IdentityServer
         };
 
         public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
-                
-                   };
+            new IdentityResource[]
+            {
+                // This scope declare which data in token (ProjectDevelopment.txt step 16.1).
+                new IdentityResources.OpenId(), // This line must be declared.
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+                new IdentityResource() { 
+                    Name = "roles",
+                    DisplayName = "Roles",
+                    Description = "User roles",
+                    UserClaims = new []{"role"}
+                },
+            };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
@@ -45,9 +55,28 @@ namespace XProject.IdentityServer
                     ClientName = "Asp.Net Core MVC",
                     ClientId = "WebMvcClient",
                     ClientSecrets = {new Secret("secret".Sha256())},
-                    AllowedGrantTypes = GrantTypes.ClientCredentials, // When you use not user apis
+                    AllowedGrantTypes = GrantTypes.ClientCredentials, // When you use not user apis and client credentials not include refresh token.
                     AllowedScopes= {"catalog_fullpermission", "photo_stock_fullpermission", IdentityServerConstants.LocalApi.ScopeName}
-                }
+                },
+                // Resource owner grand type credential workflow client declaration (ProjectDevelopment.txt step 16.2)
+                new Client
+                {
+                    ClientName = "Asp.Net Core MVC",
+                    ClientId = "WebMvcClientForUser",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword, // When you use resource owner grand type credential workflow and this type include refresh token.
+                    AllowedScopes= {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.OfflineAccess, // This line for refresh token.
+                        "roles",                        
+                    },
+                    AccessTokenLifetime = 1*60*60, // Default 1 hour.
+                    RefreshTokenExpiration = TokenExpiration.Absolute,
+                    AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddDays(60) - DateTime.Now).TotalSeconds, // Set refresh token valid time (for this sample 60 day).
+                    RefreshTokenUsage = TokenUsage.ReUse // The refresh token reusable in lifetime?
+                },
             };
     }
 }
